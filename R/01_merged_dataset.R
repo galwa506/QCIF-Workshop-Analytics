e6a45af08a1690594b6088f1771202167082c365
Galey Wangmo <galeywangmo@Mac.home>
2025-11-05 13:38:13 +1000

    Merged attendance and waitlist datasets (2024+2025)

--- R/01_merged_dataset.R
+++ R/01_merged_dataset.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(openxlsx)
 library(here)
 
-# Helper function to read and clean each csv
+# ---- READ AND CLEAN FILES ----
 read_and_clean <- function(file) {
   data <- readr::read_csv(file)
   
@@ -13,7 +13,12 @@ read_and_clean <- function(file) {
   return(data)
 }
 
-# General helper function to load and clean data for any year
+# Save merged outputs
+output_dir <- here("output")
+dir.create(output_dir, showWarnings = FALSE)
+
+
+# ---- LOAD WORKSHOP DATA ----
 load_workshop_data <- function(year, base_dir = "data") {
   
   # Build dir path
@@ -32,10 +37,6 @@ load_workshop_data <- function(year, base_dir = "data") {
   # Read and bind rows for both attendance and waitlist
   attendance_data <- map_dfr(attendance_files, read_and_clean)
   waitlist_data   <- map_dfr(waitlist_files, read_and_clean)
-  
-  # ---- SAVE MERGED OUTPUTS ----
-  output_dir <- here("output")
-  dir.create(output_dir, showWarnings = FALSE)
   
   if (nrow(attendance_data) > 0) {
     readr::write_csv(attendance_data, file.path(output_dir, paste0("attendance_", year, ".csv")))
@@ -63,3 +64,26 @@ waitlist_data_2024   <- data_2024$waitlist
 attendance_data_2025 <- data_2025$attendance
 waitlist_data_2025   <- data_2025$waitlist
 
+# Compare attendance columns
+setdiff(names(attendance_data_2024), names(attendance_data_2025))
+setdiff(names(attendance_data_2025), names(attendance_data_2024))
+
+# Compare waitlist columns
+setdiff(names(waitlist_data_2024), names(waitlist_data_2025))
+setdiff(names(waitlist_data_2025), names(waitlist_data_2024))
+
+# Combine attendance data from 2024 and 2025
+combined_attendance <- bind_rows(
+  attendance_data_2024 %>% mutate(year = 2024),
+  attendance_data_2025 %>% mutate(year = 2025)
+)
+
+# Combine waitlist data from 2024 and 2025
+combined_waitlist <- bind_rows(
+  waitlist_data_2024 %>% mutate(year = 2024),
+  waitlist_data_2025 %>% mutate(year=2025)
+)
+
+# Export final merged attendance and waitlist datasets to output directory
+readr::write_csv(combined_attendance, file.path(output_dir, "attendance_all_years.csv"))
+readr::write_csv(combined_waitlist, file.path(output_dir, "waitlist_all_years.csv"))
